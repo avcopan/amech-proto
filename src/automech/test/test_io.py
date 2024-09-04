0d78cfa81d2820c9739ea3472585a9b1cbe1d160
Andreas Copan <avcopan@gmail.com>
2024-09-04 10:21:28 -0400

    Refactor: I/O tests for mechanalyzer and RMG
    Also, adds syngas test for mechanalyzer.
    
    Currently only testing the read...

--- src/automech/test/test_io.py
+++ src/automech/test/test_io.py
@@ -11,33 +11,33 @@ DATA_PATH = Path(__file__).parent / "data"
 TEMP_PATH = tempfile.gettempdir()
 
 
-def check_counts(mech, ref_rcount, ref_scount):
+def check_counts(mech, ref_nrxns, ref_nspcs):
     """Check that the reaction and species counts are correct.
 
     :param mech: A mechanism object
-    :param ref_rcount: The correct reaction count
-    :param ref_scount: The correct species count
+    :param ref_nrxns: The correct reaction count
+    :param ref_nspcs: The correct species count
     """
-    rcount = automech.reaction_count(mech)
-    scount = automech.species_count(mech)
-    assert rcount == ref_rcount, f"{rcount} != {ref_rcount}"
-    assert scount == ref_scount, f"{scount} != {ref_scount}"
+    nrxns = automech.reaction_count(mech)
+    nspcs = automech.species_count(mech)
+    assert nrxns == ref_nrxns, f"{nrxns} != {ref_nrxns}"
+    assert nspcs == ref_nspcs, f"{nspcs} != {ref_nspcs}"
 
 
 @pytest.mark.parametrize(
-    "mech_file_name,rcount,scount",
+    "mech_file_name, nrxns, nspcs",
     [
         ("butane.dat", 101, 76),
-        ("LLNL_C2H4_mech.dat", 26, 31),
+        ("ethylene.dat", 26, 31),
     ],
 )
-def test__chemkin(mech_file_name, rcount, scount):
-    """Test automech.io.chemkin.read."""
+def test__chemkin(mech_file_name, nrxns, nspcs):
+    """Test automech.io.chemkin."""
     # Read
     mech_path = Path(DATA_PATH) / mech_file_name
     mech = automech.io.chemkin.read.mechanism(mech_path)
     print(mech)
-    check_counts(mech, ref_rcount=rcount, ref_scount=scount)
+    check_counts(mech, ref_nrxns=nrxns, ref_nspcs=nspcs)
 
     # Write
     out_mech_path = Path(TEMP_PATH) / mech_file_name
@@ -45,34 +45,48 @@ def test__chemkin(mech_file_name, rcount, scount):
     print(mech_str)
     #   - Check the string output
     mech = automech.io.chemkin.read.mechanism(mech_str)
-    check_counts(mech, ref_rcount=rcount, ref_scount=scount)
+    check_counts(mech, ref_nrxns=nrxns, ref_nspcs=nspcs)
     #   - Check the file output
     mech = automech.io.chemkin.read.mechanism(out_mech_path)
-    check_counts(mech, ref_rcount=rcount, ref_scount=scount)
+    check_counts(mech, ref_nrxns=nrxns, ref_nspcs=nspcs)
 
 
-def test__mechanalyzer__read():
+@pytest.mark.parametrize(
+    "rxn_file_name, spc_file_name, nrxns, nspcs",
+    [
+        ("propyl.dat", "propyl_species.csv", 8, 12),
+        ("syngas.dat", "syngas_species.csv", 78, 18),
+    ],
+)
+def test__mechanalyzer(rxn_file_name, spc_file_name, nrxns, nspcs):
     """Test automech.io.mechanalyzer."""
-    rxn_path = Path(DATA_PATH) / "propyl.dat"
-    spc_path = Path(DATA_PATH) / "propyl_species.csv"
+    # Read
+    rxn_path = Path(DATA_PATH) / rxn_file_name
+    spc_path = Path(DATA_PATH) / spc_file_name
     mech = automech.io.mechanalyzer.read.mechanism(rxn_path, spc_path)
-    print(rxn_path)
-    print(spc_path)
     print(mech)
+    check_counts(mech, ref_nrxns=nrxns, ref_nspcs=nspcs)
 
 
-def test__rmg__read():
+@pytest.mark.parametrize(
+    "rxn_file_name, spc_file_name, nrxns, nspcs",
+    [
+        ("cyclopentene.dat", "cyclopentene_species.txt", 100, 63),
+    ],
+)
+def test__rmg(rxn_file_name, spc_file_name, nrxns, nspcs):
     """Test automech.io.rmg."""
-    rxn_path = Path(DATA_PATH) / "cyclopentene.inp"
-    spc_path = Path(DATA_PATH) / "cyclopentene_species.txt"
+    # Read
+    rxn_path = Path(DATA_PATH) / rxn_file_name
+    spc_path = Path(DATA_PATH) / spc_file_name
     mech = automech.io.rmg.read.mechanism(rxn_path, spc_path)
-    print(rxn_path)
-    print(spc_path)
     print(mech)
+    check_counts(mech, ref_nrxns=nrxns, ref_nspcs=nspcs)
 
 
 if __name__ == "__main__":
     # test__chemkin__read("butane.dat", 101, 76)
-    test__chemkin("LLNL_C2H4_mech.dat", 26, 31)
-    # test__mechanalyzer__read("/home/avcopan/code/amech-proto/src/automech/test/data")
-    # test__rmg__read("/home/avcopan/code/amech-proto/src/automech/test/data")
+    # test__chemkin("LLNL_C2H4_mech.dat", 26, 31)
+    # test__mechanalyzer("propyl.dat", "propyl_species.csv", 8, 12)
+    test__mechanalyzer("syngas.dat", "syngas_species.csv", 78, 18)
+    # test__rmg("cyclopentene.inp", "cyclopentene_species.txt", 100, 63)
