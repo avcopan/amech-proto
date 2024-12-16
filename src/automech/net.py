4ed7f2fdedb39013a180d2bf9b3eb35f18807a76
Andreas Copan <avcopan@gmail.com>
2024-12-16 14:58:32 -0500

    New: Decomposition into sub-PES networks

--- src/automech/net.py
+++ src/automech/net.py
@@ -5,6 +5,7 @@ from collections.abc import Sequence
 from pathlib import Path
 
 import automol
+import more_itertools as mit
 import networkx
 import pyvis
 
@@ -39,13 +40,13 @@ Edge = REdge | SEdge
 Data = dict[str, object]
 NodeDatum = tuple[Node, Data]
 EdgeDatum = tuple[Node, Node, Data]
-EdgeKeyDatum = tuple[Node, Node, int, Data]
+MultiEdgeDatum = tuple[Node, Node, int, Data]
 
 
 # constructors
 def from_data(
     node_data: list[Node | NodeDatum],
-    edge_data: list[Edge | EdgeDatum | EdgeKeyDatum],
+    edge_data: list[Edge | EdgeDatum | MultiEdgeDatum],
     aux_data: Data,
 ) -> Network:
     """Construct a network from data.
@@ -61,6 +62,17 @@ def from_data(
     return net
 
 
+def copy(net: Network) -> Network:
+    """Get a copy of a network.
+
+    :param net: A network
+    :return: A copy of the network
+    """
+    return from_data(
+        node_data=node_data(net), edge_data=edge_data(net), aux_data=auxiliary_data(net)
+    )
+
+
 # getters
 def nodes(net: Network) -> list[Node]:
     """Get the list of nodes in the network.
@@ -71,13 +83,28 @@ def nodes(net: Network) -> list[Node]:
     return list(net.nodes)
 
 
-def edges(net: Network) -> list[Edge]:
+def edges(net: Network, edges: Sequence[Edge] | None = None) -> list[Edge]:
     """Get the list of edges in the network.
 
+    Includes the keys for each edge.
+
     :param net: A network
+    :param es: Optionally, validate a set of keys
     :return: The nodes
     """
-    return list(net.edges)
+
+    def _edges(edge: Edge) -> list[Edge]:
+        edge = tuple(edge)
+        if len(edge) == 3:
+            assert edge in net.edges
+            return [edge]
+        return [(*edge, k) for k in net.get_edge_data(*edge)]
+
+    return (
+        list(net.edges)
+        if edges is None
+        else list(mit.unique_everseen(itertools.chain(*map(_edges, edges))))
+    )
 
 
 def node_data(net: Network) -> list[NodeDatum]:
@@ -89,7 +116,7 @@ def node_data(net: Network) -> list[NodeDatum]:
     return list(net.nodes.data())
 
 
-def edge_data(net: Network) -> list[EdgeKeyDatum]:
+def edge_data(net: Network) -> list[MultiEdgeDatum]:
     """Get the data associated with each edge.
 
     :param net: A network
@@ -112,7 +139,8 @@ def set_nodes(net: Network, node_data: list[Node | NodeDatum]) -> Network:
     """Get the list of nodes in the network.
 
     :param net: A network
-    :return: The nodes
+    :param node_data: Node data
+    :return: The updated network
     """
     return from_data(
         node_data=node_data, edge_data=edge_data(net), aux_data=auxiliary_data(net)
@@ -123,21 +151,153 @@ def set_edges(net: Network, edge_data: list[Edge | EdgeDatum]) -> Network:
     """Get the list of edges in the network.
 
     :param net: A network
-    :return: The nodes
+    :param edge_data: Edge data
+    :return: The updated network
     """
-    pass
+    return from_data(
+        node_data=node_data(net), edge_data=edge_data, aux_data=auxiliary_data(net)
+    )
 
 
-def set_network_data(net: Network, data: dict[str, object]) -> Network:
+def set_auxiliary_data(net: Network, aux_data: dict[str, object]) -> Network:
     """Get additional data associated with a network.
 
     :param net: A network
-    :return: The additional data
+    :param aux_data: Auxiliary data
+    :return: The updated network
     """
-    pass
+    return from_data(
+        node_data=node_data(net), edge_data=edge_data(net), aux_data=aux_data
+    )
+
+
+# properties
+def species_names(net: Network) -> list[str]:
+    """Get the list of species names in a network.
+
+    Only includes the names of species that are in nodes.
+
+    :param net: A network
+    :return: The species names
+    """
+    return sorted(
+        mit.unique_everseen(
+            itertools.chain(*([n] if isinstance(n, str) else n for n in nodes(net)))
+        )
+    )
+
+
+def node_neighbors(net: Network, node: Node) -> list[Node]:
+    """Get the list of neighbors of a node.
+
+    :param net: A network
+    :param node: A node in the network
+    :return: The list of neighbors
+    """
+    return list(net[node])
+
+
+# transformations
+def add_node(
+    net: Network,
+    node: Node,
+    data: Data | None = None,
+    source_net: Network | None = None,
+) -> Network:
+    """Add a node (optionally, by pulling data from another network).
+
+    :param net: A network
+    :param node: A node to add
+    :param data: Node data
+    :param source_net: A network to pull node data from
+    :return: The updated network
+    """
+    net = copy(net)
+    data = data if data is not None else source_net.nodes[node]
+    net.add_node(node, **data)
+    return net
+
+
+def add_edge(
+    net: Network,
+    edge: Edge,
+    key: int | None = None,
+    data: Data | None = None,
+    node_data: dict[Node, Data] | None = None,
+    source_net: Network | None = None,
+) -> Network:
+    """Add an edge (optionally, by pulling data from another network).
+
+    If `key` is set, this adds a single edge.
+    Otherwise, this is assumed to add data for all edges connecting these nodes.
+
+    If pulling data from another network, data for the nodes will be added too.
+
+    :param net: A network
+    :param edge: An edge to add
+    :param data: Edge data
+    :param source_net: A network to pull edge data from
+    :return: The updated network
+    """
+    net = copy(net)
+
+    if source_net is not None:
+        node_data = {n: source_net.nodes[n] for n in edge}
+        data = source_net.get_edge_data(*edge, key=key)
+
+    data = data if key is None or data is None else {key: data}
+
+    node_data = {} if node_data is None else node_data
+    data = {} if data is None else data
+
+    # Add nodes
+    for node in edge:
+        net.add_node(node, **node_data.get(node, {}))
+
+    # Add edge(s)
+    for key_ in data:
+        net.add_edge(*edge, key=key_, **data.get(key_, {}))
+
+    return net
+
+
+def remove_nodes(net: Network, nodes: list[Node]) -> Network:
+    """Remove a list of nodes from the network.
+
+    :param net: A network
+    :param nodes: Nodes to remove
+    :return: The updated network
+    """
+    net = copy(net)
+    net.remove_nodes_from(nodes)
+    return net
+
+
+def remove_edges(net: Network, edges: list[Edge]) -> Network:
+    """Remove a list of edges from the network.
+
+    :param net: A network
+    :param edges: Edges to remove
+    :return: The updated network
+    """
+    net = copy(net)
+    net.remove_edges_from(edges)
+    return net
 
 
 # unions and subgraphs
+def union_all(nets: Sequence[Network]) -> Network:
+    """Get the union of a sequence of networks.
+
+    :param nets: A sequence of networks
+    :return: The combined network
+    """
+    net = networkx.compose_all(nets)
+    return set_auxiliary_data(
+        net, aux_data={k: v for n in nets for k, v in auxiliary_data(n).items()}
+    )
+
+
 def union(net1: Network, net2: Network) -> Network:
     """Get the union of two networks.
 
@@ -148,76 +308,64 @@ def union(net1: Network, net2: Network) -> Network:
     return union_all([net1, net2])
 
 
-def union_all(nets: Sequence[Network]) -> Network:
-    """Get the union of a sequence of networks.
-
-    :param nets: A sequence of networks
-    :return: The combined network
-    """
-    net: Network = networkx.compose_all(nets)
-    net.graph = {k: v for n in nets for k, v in dict(n.graph).items()}
-    return net
-
-
-def subnetwork(net: Network, keys: Sequence[str]) -> Network:
+def subnetwork(net: Network, ns: Sequence[Node], aux: bool = False) -> Network:
     """Extract a node-induced sub-network from a network.
 
     :param net: A network
     :param keys: A sequence of node keys
+    :param aux: Pass along auxiliary data?
     :return: The network
     """
-    sub_net: Network = networkx.subgraph(net, keys).copy()
-    sub_net.graph = dict(net.graph).copy()
-    return sub_net
+    sub_net = networkx.subgraph(net, ns)
+    return from_data(
+        node_data=node_data(sub_net),
+        edge_data=edge_data(sub_net),
+        aux_data=auxiliary_data(net) if aux else {},
+    )
 
 
-def edge_subnetwork(net: Network, keys: Sequence[tuple[str, str]]) -> Network:
+def edge_subnetwork(net: Network, es: Sequence[Edge], aux: bool = False) -> Network:
     """Extract a node-induced sub-network from a network.
 
     :param net: A network
     :param keys: A sequence of edge keys
+    :param aux: Pass along auxiliary data?
     :return: The network
     """
-    sub_net: Network = networkx.edge_subgraph(net, keys).copy()
-    sub_net.graph = dict(net.graph).copy()
-    return sub_net
+    sub_net = networkx.edge_subgraph(net, edges(net, edges=es))
+    return from_data(
+        node_data=node_data(sub_net),
+        edge_data=edge_data(sub_net),
+        aux_data=auxiliary_data(net) if aux else {},
+    )
 
 
-def connected_components(net: Network) -> list[Network]:
+def connected_components(net: Network, aux: bool = False) -> list[Network]:
     """Determine the connected components of a network.
 
     :param net: A network
+    :param aux: Pass along auxiliary data?
     :return: The connected components
     """
-    sub_nets = [subnetwork(net, ks) for ks in networkx.connected_components(net)]
-    for sub_net in sub_nets:
-        sub_net.graph = dict(net.graph).copy()
-    return sub_nets
+    return [subnetwork(net, ks, aux=aux) for ks in networkx.connected_components(net)]
 
 
-# properties
-def species_names(net: Network) -> list[str]:
-    """Get the list of species names in a network.
-
-    :param net: A network
-    :return: The species names
-    """
-
-
-def isolates(net: Network) -> Network:
+def isolates(net: Network, aux: bool = False) -> Network:
     """Get isolated species as a "network".
 
     :param net: A network
+    :param aux: Pass along auxiliary data?
     :return: The isolated species
     """
-    return subnetwork(net, networkx.isolates(net))
+    return subnetwork(net, networkx.isolates(net), aux=aux)
 
 
-def pes_network(net: Network, formula: dict | str) -> Network:
+def pes_network(net: Network, formula: dict | str, aux: bool = False) -> Network:
     """Select the network associated with a specific PES.
 
     :param net: A network
     :param formula: The formula to select
+    :param aux: Pass along auxiliary data?
     :return: The PES network
     """
     fml = automol.form.from_string(formula) if isinstance(formula, str) else formula
@@ -226,39 +374,73 @@ def pes_network(net: Network, formula: dict | str) -> Network:
         for *k, d in edge_data(net)
         if automol.form.equal(d[Reaction.formula], fml)
     ]
-    return edge_subnetwork(net, edge_keys)
+    return edge_subnetwork(net, edge_keys, aux=aux)
 
 
-def pes_networks(net: Network, with_isolates: bool = False) -> list[Network]:
+def pes_networks(
+    net: Network, with_isolates: bool = False, aux: bool = False
+) -> list[Network]:
     """Determine the PES networks in a larger network.
 
     :param net: A network
     :param with_isolates: Whether to include isolated species as a "network"
+    :param aux: Pass along auxiliary data?
     :return: The PES component networks
     """
     fmls = automol.form.unique([d[Reaction.formula] for *_, d in edge_data(net)])
-    nets = [pes_network(net, f) for f in fmls]
+    nets = [pes_network(net, f, aux=aux) for f in fmls]
     if with_isolates:
         nets.append(isolates(net))
     return nets
 
 
-# def subpes_networks(net: Network, with_isolates: bool = False) -> list[Network]:
-#     """Determine the PES networks in a larger network.
+def subpes_networks(
+    net: Network, with_isolates: bool = False, aux: bool = False
+) -> list[Network]:
+    """Determine the PES networks in a larger network.
 
-#     :param net: A network
-#     :param with_isolates: Whether to include isolated species as a "network"
-#     :return: The PES component networks
-#     """
-#     net0 = copy.deepcopy(net)
+    :param net: A network
+    :param with_isolates: Whether to include isolated species as a "network"
+    :param aux: Pass along auxiliary data?
+    :return: The PES component networks
+    """
+    # Set up empty list of sub-PES networks
+    nets = []
 
-#     bimol_dct = {n: net0[n] for n in nodes(n) if len(n) > 1}
-#     net = net.remove_nodes_from(bimol_dct.keys())
-#     print(bimol_dct)
+    # Determine the sub-PES networks among the unimolecular nodes
+    multi_nodes = [n for n in nodes(net) if len(n) > 1]
+    net_uni = remove_nodes(net, multi_nodes)
+    nets.extend(connected_components(net_uni, aux=aux))
+
+    # Partition the missing edges of the non-unimolecular nodes into those connected to
+    # unimolecular nodes and those connected to other non-unimolecular nodes
+    multi_to_uni_edges = [
+        (n1, n2) for n1 in multi_nodes for n2 in node_neighbors(net, n1) if len(n2) == 1
+    ]
+    multi_to_multi_edges = [
+        (n1, n2) for n1 in multi_nodes for n2 in node_neighbors(net, n1) if len(n2) > 1
+    ]
+
+    # For each multi-to-unimolecular edge, simply add it to the appropriate sub-PES
+    for multi_node, uni_node in multi_to_uni_edges:
+        i, x = next((i, x) for i, x in enumerate(nets) if uni_node in nodes(x))
+        nets[i] = add_edge(x, (multi_node, uni_node), source_net=net)
+
+    for edge in multi_to_multi_edges:
+        i, x = next(
+            ((i, x) for i, x in enumerate(nets) if all(n in nodes(x) for n in edge)),
+            (None, None),
+        )
+        if i is None:
+            nets.append(edge_subnetwork(net, [edge], aux=True))
+        else:
+            nets[i] = add_edge(x, edge, source_net=net)
+
+    return nets
 
 
 # serialization
-def dict_(net: Network) -> dict[object, object]:
+def dict_(net: Network) -> Data:
     """Serialize a network as a string.
 
     :param net: A network
